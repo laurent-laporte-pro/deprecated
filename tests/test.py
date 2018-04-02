25bcc46823bd4344a0e82361aa3218207cf25e2f
Laurent LAPORTE <tantale.solutions@gmail.com>
2018-04-02 16:49:50 +0200

    Fix unit test: The classes ``SetuptoolsVersion`` and ``SetuptoolsLegacyVersion`` are removed since setuptools >= 39. They are replaced by ``Version`` and ``LegacyVersion`` respectively. To check if the version is good, we now use the solution explained here: https://github.com/pypa/setuptools/issues/1299.

--- tests/test.py
+++ tests/test.py
@@ -14,4 +14,14 @@ def test_deprecated_has_version():
     # The deprecated package must have a valid version number
     assert deprecated.__version__ is not None
     version = pkg_resources.parse_version(deprecated.__version__)
-    assert not isinstance(version, pkg_resources.SetuptoolsLegacyVersion)
+
+    # .. note::
+    #
+    #    The classes ``SetuptoolsVersion`` and ``SetuptoolsLegacyVersion``
+    #    are removed since setuptools >= 39.
+    #    They are replaced by ``Version`` and ``LegacyVersion`` respectively.
+    #
+    #    To check if the version is good, we now use the solution explained here:
+    #    https://github.com/pypa/setuptools/issues/1299
+
+    assert 'Legacy' not in version.__class__.__name__

eaf51bf76b3fba151eafd54a6911601d3f95c2a4
Laurent LAPORTE <tantale.solutions@gmail.com>
2019-07-06 15:01:20 +0200

    Prepare next version 1.2.6 (unreleased)

--- setup.py
+++ setup.py
@@ -143,7 +143,7 @@ from setuptools import setup
 
 setup(
     name='Deprecated',
-    version='1.2.5',
+    version='1.2.6',
     url='https://github.com/tantale/deprecated',
     project_urls={
         "Documentation": "https://deprecated.readthedocs.io/en/latest/",

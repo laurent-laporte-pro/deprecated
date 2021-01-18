5776579ffa5ca58459a07ccde925729c1fb007af
Laurent LAPORTE <tantale.solutions@gmail.com>
2021-01-18 14:03:52 +0100

    Correct classifiers in ``setup.py``.

--- setup.py
+++ setup.py
@@ -178,7 +178,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
+        # 'Programming Language :: Python :: 3.10',  # scheduled on 2021-10-04
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     extras_require={

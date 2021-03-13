e84ec7774c60eb35c295840f07a951a025ef430f
Laurent LAPORTE <tantale.solutions@gmail.com>
2021-03-13 15:00:11 +0100

    New bug fix release v1.2.12 (2020-03-13)

--- deprecated/__init__.py
+++ deprecated/__init__.py
@@ -9,7 +9,7 @@ Python ``@deprecated`` decorator to deprecate old python classes, functions or m
 
 __version__ = "1.2.12"
 __author__ = u"Laurent LAPORTE <tantale.solutions@gmail.com>"
-__date__ = "unreleased"
+__date__ = "2020-03-13"
 __credits__ = "(c) Laurent LAPORTE"
 
 from deprecated.classic import deprecated

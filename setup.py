650443d2caa0ac67aeb4c86b50492c7eaa970a28
mjhajharia <meenal@mjhajharia.com>
2021-11-02 00:45:49 +0530

    something

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='deprecator',
-    version='1.0',
+    version='1.0.0',
     url='https://github.com/mjhajharia/deprecator',
     license='MIT',
     author='Meenal Jhajharia',  # since v1.1.0

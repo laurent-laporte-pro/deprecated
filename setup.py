6808dcdd9a47d37e9177faa65c5d94621f37742a
Tomasz Kłoczko <kloczek@github.com>
2024-03-09 18:06:19 +0000

    update list of supported python versions
    Signed-off-by: Tomasz Kłoczko <kloczek@github.com>

--- setup.py
+++ setup.py
@@ -168,13 +168,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
@@ -193,5 +186,5 @@ setup(
             'jinja2~=3.0.3',
         ]
     },
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    python_requires='>=3.8',
 )

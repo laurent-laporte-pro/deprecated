c9802b495849bc5745d25aaf094af7b905bc536d
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2024-11-15 14:12:22 +0100

    docs: change the author email in project metadata

--- deprecated/__init__.py
+++ deprecated/__init__.py
@@ -8,7 +8,7 @@ Python ``@deprecated`` decorator to deprecate old python classes, functions or m
 """
 
 __version__ = "1.2.14"
-__author__ = u"Laurent LAPORTE <tantale.solutions@gmail.com>"
+__author__ = u"Laurent LAPORTE <laurent.laporte.pro@gmail.com>"
 __date__ = "2023-05-27"
 __credits__ = "(c) Laurent LAPORTE"
 

0d4083f7927686bcf0b52840dd8393499718f9c6
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2024-11-15 14:51:47 +0200

    Add support for Python 3.13

--- setup.py
+++ setup.py
@@ -182,6 +182,7 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     extras_require={

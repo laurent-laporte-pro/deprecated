a6e6d73c3a605c3e032a574ba189e69ce1991c59
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-01-20 18:05:15 +0100

    docs: fix compat with sphinx8
    Fixes errors caused by invalid intersphinx mappings.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -173,9 +173,9 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    'https://docs.python.org/3/': None,
-    'https://wrapt.readthedocs.io/en/latest/': None,
-    'http://flask.pocoo.org/docs/1.0/': None,
+    'python': ('https://docs.python.org/3/', None),
+    'wrapt': ('https://wrapt.readthedocs.io/en/latest/', None),
+    'flask': ('http://flask.pocoo.org/docs/1.0/', None),
     'django': ('https://docs.djangoproject.com/en/2.1/', 'https://docs.djangoproject.com/en/2.1/_objects/'),
 }
 

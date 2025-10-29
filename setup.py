a08ab190cc7ac4398d432167c3d84ff95aacdf7b
Laurent LAPORTE <laurent@dfya.io>
2025-10-29 14:57:16 +0100

    docs: update compatibility matrix of Wrapt for Python 3.13 and 3.14

--- setup.py
+++ setup.py
@@ -182,6 +182,8 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     extras_require={

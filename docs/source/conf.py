966df9d554ebc5bd531ec3e6cde64f29aeef9fab
Laurent LAPORTE <laurent@dfya.io>
2025-10-30 08:39:19 +0100

    Prepare next version 1.3.1 (unreleased)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -61,7 +61,7 @@ author = 'Marcos CARDOSO & Laurent LAPORTE'
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = "1.3.0"
+release = "1.3.1"
 # The short X.Y version.
 version = release.rpartition('.')[0]
 
